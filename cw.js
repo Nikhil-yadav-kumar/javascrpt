38afda9722a0058260304e61bc1c0bd94ad162f8
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2026-07-06 15:18:15 +0530

    fetchApi

--- cw.js
+++ cw.js
@@ -147,3 +147,9 @@
 
 
 
+async function getposts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await response.json();
+    console.log(data);      
+}
+getposts();
