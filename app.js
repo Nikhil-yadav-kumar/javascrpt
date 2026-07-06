6dc2aa32d877db1723e35b64d8db9aa048f07db1
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2026-07-06 15:25:43 +0530

    export_Import

---
+++ app.js
@@ -0,0 +1,3 @@
+import{add, PI} from './mathsUtils.js';
+ console.log(add(6,7));
+ console.log(PI)
---
+++ mathsUtils.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a + b;
+}
+const PI=3.14159;
+
+export { add, PI };
